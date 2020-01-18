2a20d31ca60388b7eb2ff5b3c7aad661e5327ee6
Kei178 <keisuke.inaba.cpa@gmail.com>
2020-01-18 16:09:08 +1000

    Fixed Refused to connect...

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -16,6 +16,23 @@
 #   # policy.report_uri "/csp-violation-report-endpoint"
 # end
 
+# To solve: `Refused to connect to http://localhost:3035/sockjs-node/info...`
+Rails.application.config.content_security_policy do |p|
+  p.font_src    :self, :https, :data
+  p.img_src     :self, :https, :data
+  p.object_src  :none
+  p.style_src   :self, :https, :unsafe_inline
+
+  if Rails.env.development?
+    p.script_src :self, :https, :unsafe_eval
+    p.default_src :self, :https, :unsafe_eval
+    p.connect_src :self, :https, 'http://localhost:3035', 'ws://localhost:3035'
+  else
+    p.script_src :self, :https
+    p.default_src :self, :https
+  end
+end
+
 # If you are using UJS then enable automatic nonce generation
 # Rails.application.config.content_security_policy_nonce_generator = -> request { SecureRandom.base64(16) }
 
