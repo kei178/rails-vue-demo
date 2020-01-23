1fbb0e5a6e77c92a7f8f349da264635055bc4497
Kei178 <keisuke.inaba.cpa@gmail.com>
2020-01-24 08:44:48 +1000

    Update circleci

--- .circleci/setup_heroku.sh
+++ .circleci/setup_heroku.sh
@@ -10,10 +10,10 @@ cat > ~/.netrc << EOF
 machine api.heroku.com
   login $HEROKU_LOGIN
   password $HEROKU_API_KEY
+machine git.heroku.com
+  login $HEROKU_LOGIN
+  password $HEROKU_API_KEY
 EOF
-# machine git.heroku.com
-#   login $HEROKU_LOGIN
-#   password $HEROKU_API_KEY
 
 # Add heroku.com to the list of known hosts
 ssh-keyscan -H heroku.com >> ~/.ssh/known_hosts
